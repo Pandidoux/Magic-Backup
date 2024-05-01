c714d0d5ddaa7c2a0a125291949986b6d5ed7833
Pandidoux <louis.caudrelier.1459@gmail.com>
2024-05-01 21:57:58 +0200

    handle single and multiple files backups

--- magic_backup.sh
+++ magic_backup.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-# User-defined variables
-debug_mode=false
-backup_directory="/mnt/sharing/backups/$(hostname)"
-files_to_backup=(
+# Backup configuration
+backup_directory="/mnt/sharing/backups/$(hostname)" # Directory where backup files are stored
+elements_to_backup=( # List of files and/or directory to backup
     "/home/user/docker-volumes"
     "/home/user/scripts"
     "/etc/cron.daily"
     "/etc/cron.hourly"
 )
-days_to_keep=7
-shutdown_containers=true
-container_filter_include_exclude="exclude"
-container_filter=(
+onefile_backup=true # true: only one backup file, false: backup file for each element
+# Compression
+compression_enabled=true # Compressed backup
+compression_level="9" # Compression level from 1 to 9
+# Docker
+stop_containers=false # Stop docker containers
+container_filter_type="include" # "include" to only stop listed container, "exclude" to keep only listed container running
+container_filter=( # List of container names
     "portainer_agent"
     "ubuntu"
 )
-log_file="/var/log/magic_backups.log"
+# Detete old backups
+days_to_keep=7 # Older backup will be deleted, 0 to disable
+# Logs
+use_logfile=true # Write logs in a file
+log_file="/var/log/magic_backups.log" # File for the logs
+# Debug
+debug_mode=false # Debug mode, do nothing but print actions
 
 # ==================================================
 
@@ -24,8 +33,10 @@ log_file="/var/log/magic_backups.log"
 log() {
     current_date="$(date +"%Y-%m-%d %T")"
     echo "$current_date - $1"
-    if [ "$debug_mode" = false ]; then
-        echo "$current_date - $1" >>"$log_file"
+    if [ "$use_logfile" = true ]; then
+        if [ "$debug_mode" = false ]; then
+            echo "$current_date - $1" >>"$log_file"
+        fi
     fi
 }
 
@@ -40,7 +51,7 @@ stop_docker_containers() {
             container_grep_filter="$container_grep_filter|$container"
         fi
     done
-    if [ "$container_filter_include_exclude" = "include" ]; then # Include container filter
+    if [ "$container_filter_type" = "include" ]; then # Include container filter
         log "Docker stopping containers => $container_grep_filter"
         if [ "$debug_mode" = false ]; then
             sudo docker stop $(sudo docker ps --all --format '{{.Names}}' | grep --extended-regexp "$container_grep_filter") >/dev/null 2>>"$log_file"
@@ -67,7 +78,7 @@ start_docker_containers() {
             container_grep_filter="$container_grep_filter|$container"
         fi
     done
-    if [ "$container_filter_include_exclude" = "include" ]; then # Include container filter
+    if [ "$container_filter_type" = "include" ]; then # Include container filter
         log "Docker starting containers => $container_grep_filter"
         if [ "$debug_mode" = false ]; then
             sudo docker start $(sudo docker ps --all --format '{{.Names}}' | grep --extended-regexp "$container_grep_filter") >/dev/null 2>>"$log_file"
@@ -103,47 +114,79 @@ perform_backup() {
     # Verify if backup directory is writable
     if [ ! -w "$backup_directory" ]; then
         log "Error: Backup directory is not writable => $backup_directory"
-        return 1
+        if [ "$debug_mode" = false ]; then
+            return 1
+        fi
     fi
 
-    use_pigz=false
-    if command -v pigz &>/dev/null; then # Use pigz for compression
-        use_pigz=true
-        log "Backup using pigz for multi-core compression"
-    else
-        log "Backup using gzip for single-core compression"
+    # compression_arg=""
+    if [ "$compression_enabled" = true ]; then
+        compression_program="gzip"
+        if command -v pigz &>/dev/null; then # Use pigz for compression
+            compression_program="pigz"
+            log "Backup using pigz for multi-core compression"
+        else
+            log "Backup using gzip for single-core compression"
+        fi
+        # compression_arg="--use-compress-program=${compression_program} -${compression_level}"
     fi
 
-    # Loop through each file/folder to backup
-    for item in "${files_to_backup[@]}"; do
-        if [ -e "$item" ]; then
-            backup_filename="${item##*/}_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" # Append date to filename
-            log "Backup $item => $backup_directory/$backup_filename"
-            if [ "$debug_mode" = false ]; then
-                if [ "$use_pigz" = true ]; then # Use pigz for compression
-                    tar --create --use-compress-program="pigz -9" -f "$backup_directory/$backup_filename" "$item" >/dev/null 2>>"$log_file"
-                    if [ $? -ne 0 ] && [ "$backup_ok" = true ]; then
-                        backup_ok=false
-                    fi
-                else # Use gzip for compression
-                    tar --create --use-compress-program="gzip -9" -f "$backup_directory/$backup_filename" "$item" >/dev/null 2>>"$log_file"
-                    if [ $? -ne 0 ] && [ "$backup_ok" = true ]; then
-                        backup_ok=false
-                    fi
+    if [ "$onefile_backup" = true ]; then
+        # for item in "${elements_to_backup[@]}"; do # Loop through each file/folder to backup
+        #     if [ -e "$item" ]; then
+        #         items_to_backup="${items_to_backup} ${item}"
+        #     else
+        #         log "Warning: File or directory $item does not exist. Skipping..."
+        #     fi
+        # done
+        backup_filename="Backup_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" # Append date to filename
+        log "Backup $item => ${backup_directory}/${backup_filename}"
+        if [ "$debug_mode" = false ]; then
+            # tar --create $compression_arg --exclude-vcs -f "${backup_directory}/${backup_filename}" $items_to_backup >/dev/null 2>>"${log_file}"
+            if [ "$compression_enabled" = true ]; then
+                tar --create --exclude-vcs "${elements_to_backup[@]}" | $compression_program -${compression_level} >"${backup_directory}/${backup_filename}" 2>>"${log_file}"
+            else
+                tar --create --exclude-vcs -f "${backup_directory}/${backup_filename}" "${elements_to_backup[@]}" 2>>"${log_file}"
+            fi
+            if [ $? -ne 0 ]; then
+                log "ERROR: An error as occured !"
+                if [ "$backup_ok" = true ]; then
+                    backup_ok=false
                 fi
             fi
-        else
-            log "Warning: File or directory $item does not exist. Skipping..."
         fi
-    done
+    else
+        for item in "${elements_to_backup[@]}"; do # Loop through each file/folder to backup
+            if [ -e "$item" ]; then
+                backup_filename="${item##*/}_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" # Append date to filename
+                log "Backup $item => ${backup_directory}/${backup_filename}"
+                if [ "$debug_mode" = false ]; then
+                    # tar --create $compression_arg --exclude-vcs -f "${backup_directory}/${backup_filename}" "${item}" >/dev/null 2>>"${log_file}"
+                    if [ "$compression_enabled" = true ]; then
+                        tar --create --exclude-vcs "${item}" | $compression_program -${compression_level} >"${backup_directory}/${backup_filename}" 2>>"${log_file}"
+                    else
+                        tar --create --exclude-vcs -f "${backup_directory}/${backup_filename}" "${item}" 2>>"${log_file}"
+                    fi
+                    if [ $? -ne 0 ]; then
+                        log "ERROR: An error as occured !"
+                        if [ "$backup_ok" = true ]; then
+                            backup_ok=false
+                        fi
+                    fi
+                fi
+            else
+                log "Warning: File or directory $item does not exist. Skipping..."
+            fi
+        done
+    fi
 
     # log "Backup process complete."
 }
 
 # Function to delete old backups
 delete_old_backups() {
+    log "Delete files older than $days_to_keep in directory => $backup_directory"
     if [ "$backup_ok" = true ]; then
-        log "Delete files older than $days_to_keep in directory => $backup_directory"
         if [ "$debug_mode" = false ]; then
             find "$backup_directory" -type f -daystart -mtime +"$days_to_keep" -delete
         fi
@@ -158,9 +201,9 @@ delete_old_backups() {
 start_time=$(date +%s)
 backup_ok=true
 
-# Check container_filter_include_exclude value
-if [ "$container_filter_include_exclude" != "include" ] && [ "$container_filter_include_exclude" != "exclude" ]; then
-    echo "Variable container_filter_include_exclude must be 'include' or 'exclude'."
+# Check container_filter_type value
+if [ "$container_filter_type" != "include" ] && [ "$container_filter_type" != "exclude" ]; then
+    echo "Variable container_filter_type must be 'include' or 'exclude'."
     exit 1
 fi
 
@@ -171,7 +214,7 @@ if ! command -v tar &>/dev/null; then
 fi
 
 # Shut down containers if enabled
-if [ "$shutdown_containers" = true ]; then
+if [ "$stop_containers" = true ]; then
     stop_docker_containers
 fi
 
@@ -184,7 +227,7 @@ if [ "$days_to_keep" -ne 0 ]; then
 fi
 
 # Start containers if enabled
-if [ "$shutdown_containers" = true ]; then
+if [ "$stop_containers" = true ]; then
     start_docker_containers
 fi
 
